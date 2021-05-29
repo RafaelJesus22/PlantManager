83afafb2a8167fa4e77e52bb64b89b485e17ae33
RafaelJesus22 <rafael.moura22@fatec.sp.gov.br>
2021-05-28 23:29:48 -0300

    primary plant card component layout done

--- src/components/PlantCard/Primary.tsx
+++ src/components/PlantCard/Primary.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
   StyleSheet, 
-  View,
   Text
 } from 'react-native';
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler'
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { SvgFromUri } from 'react-native-svg';
 
 import colors from '../../styles/colors';
 import fonts from '../../styles/fonts';
@@ -19,14 +19,36 @@ interface PlantProps extends RectButtonProps {
 
 export const Primary = ({ data, ...rest }: PlantProps) => {
   return (
-    <RectButton style={styles.container} {...rest}>
-      {/* PAROU EM 59H11 */}
+    <RectButton 
+      style={styles.container} 
+      {...rest}
+    >
+      <SvgFromUri 
+        uri={ data.photo } 
+        height={70}
+        width={70}
+      />
+
+      <Text style={styles.text}>
+        { data.name }    
+      </Text>  
     </RectButton>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-
-  }
+    flex: 1,
+    maxWidth: '45%',
+    backgroundColor: colors.shape,
+    borderRadius: 20,
+    paddingVertical: 10,
+    alignItems: 'center',
+    margin: 10
+  },
+  text: {
+    color: colors.green_dark,
+    fontFamily: fonts.heading,
+    marginVertical: 16,
+  },
 })
