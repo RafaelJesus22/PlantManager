e76d3c540ae0c0e73d8b90b38b701e7eee6a570e
RafaelJesus22 <rafael.moura22@fatec.sp.gov.br>
2021-06-04 08:38:58 -0300

    LoadPlants function refactord

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -40,17 +40,31 @@ export const savePlant = async (plant: PlantProps): Promise<void> => {
       })
     );
   } catch (err) {
-    // throw new Error(err);
-    console.log('ERRO AO SALVAR PLANTA')
+    throw new Error(err);
   }
 }
 
-export const loadPlant = async (): Promise<StoragePlantProps> => {
+export const loadPlant = async (): Promise<PlantProps[]> => {
   try {
     const data = await AsyncStorage.getItem('@plantmanager:plants');
     const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
-    return plants;
+    const plantsSorted = Object
+    .keys(plants)
+    .map(plant => {
+      return {
+        ...plants[plant].data,
+        hour: format(new Date(plants[plant].data.dateTimeNotification), 'HH:mm')
+      }
+    })
+    .sort((a, b) =>
+      Math.floor(
+        new Date(a.dateTimeNotification).getTime() / 1000 -
+        Math.floor(new Date(b.dateTimeNotification).getTime() - 1000)
+      )
+    )
+
+    return plantsSorted;
   } catch (err) {
     throw new Error(err);
   }
